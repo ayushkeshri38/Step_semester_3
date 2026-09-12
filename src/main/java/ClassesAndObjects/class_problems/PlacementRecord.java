a23675b96a4ffe565d6ee71e8933fa025766df61
ayushkeshri38 <ayushkeshri38@gmail.com>
2026-09-12 16:10:57 +0530

    WEEK 6

---
+++ src/main/java/ClassesAndObjects/class_problems/PlacementRecord.java
@@ -0,0 +1,31 @@
+package ClassesAndObjects.class_problems;
+
+public class PlacementRecord {
+
+    String studentName;
+    String company;
+    double packageLpa;
+
+    public PlacementRecord(String studentName, String company, double packageLpa) {
+        this.studentName = studentName;
+        this.company = company;
+        this.packageLpa = packageLpa;
+    }
+
+    public void printRecord() {
+        System.out.println(studentName + " -> " + company + " @ " + packageLpa + " LPA");
+    }
+
+    public static void main(String[] args) {
+
+        PlacementRecord[] records = {
+                new PlacementRecord("Ravi", "TCS", 4.5),
+                new PlacementRecord("Anitha", "Zoho", 6.2),
+                new PlacementRecord("Karthik", "Infosys", 4.0)
+        };
+
+        for (int i = 0; i < records.length; i++) {
+            records[i].printRecord();
+        }
+    }
+}
